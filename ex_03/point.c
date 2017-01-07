3ba8e8211930229cdbad4370a124d8b30f65ed4a
Guillaume MARDON <guillaume.mardon@epitech.eu>
2017-01-07 20:15:11 +0100

    ex_03

---
+++ ex_03/point.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "point.h"
+#include "new.h"
+
+typedef struct
+{
+    Class base;
+    int x, y;
+  	char *last_to_string;
+} PointClass;
+
+static void ctor(Object* self, va_list * args)
+{
+  PointClass *point = (PointClass*) self;
+
+  point->x = va_arg(*args, int);
+  point->y = va_arg(*args, int);
+  va_end(*args);
+}
+
+static void dtor(Object* self)
+{
+  PointClass *point;
+
+  point = (PointClass*) self;
+  if (point->last_to_string)
+    free(point->last_to_string);
+}
+
+static char const *to_string(Object *self)
+{
+  PointClass *point;
+  char *str;
+  int size;
+
+  point = (PointClass*) self;
+  if (point->last_to_string)
+    free(point->last_to_string);
+  str = malloc(26 + strlen(point->base.__name__));
+  size = sprintf(str, "<%s (%d, %d)>", point->base.__name__, point->x, point->y);
+  point->last_to_string = str;
+  str[size] = 0;
+  return (str);
+}
+
+static Object *add_operator(const Object* self, const Object *other)
+{
+  PointClass *self_point;
+  PointClass *other_point;
+  PointClass *result_point;
+
+  self_point = (PointClass*) self;
+  other_point = (PointClass*) other;
+  result_point = new(Point);
+  result_point->x = self_point->x + other_point->x;
+  result_point->y = self_point->y + other_point->y;
+  return (result_point);
+}
+
+static Object *sub_operator(const Object* self, const Object *other)
+{
+  PointClass *self_point;
+  PointClass *other_point;
+  PointClass *result_point;
+
+  self_point = (PointClass*) self;
+  other_point = (PointClass*) other;
+  result_point = new(Point);
+  result_point->x = self_point->x - other_point->x;
+  result_point->y = self_point->y - other_point->y;
+  return (result_point);
+}
+
+static PointClass _description = {
+    { sizeof(PointClass), "Point", &ctor, &dtor, &to_string,
+  &add_operator, &sub_operator},
+    0, 0, NULL
+};
+
+Class* Point = (Class*) &_description;
---
+++ ex_03/vertex.c
@@ -0,0 +1,94 @@
+/*
+** vextex.c for cpp_SKL in /media/gmardon/1aa9b3b8-3e24-4ea9-9b0d-d57254b2d1b9/guillaume.mardon/delivery/cpp_SKL/ex_02/
+**
+** Made by Guillaume MARDON
+** Login   <guillaume.mardon@epitech.eu@epitech.eu>
+**
+** Started on  Sat Jan  7 18:09:19 2017 Guillaume MARDON
+** Last update Sat Jan  7 20:14:10 2017 Guillaume MARDON
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "vertex.h"
+#include "new.h"
+
+typedef struct
+{
+    Class base;
+    int x, y, z;
+  	char *last_to_string;
+} VertexClass;
+
+static void ctor(Object* self, va_list * args)
+{
+  VertexClass *vertex = (VertexClass*) self;
+
+  vertex->x = va_arg(*args, int);
+  vertex->y = va_arg(*args, int);
+  vertex->z = va_arg(*args, int);
+  va_end(*args);
+}
+
+static void dtor(Object* self)
+{
+  VertexClass *vertex;
+
+  vertex = (VertexClass *)self;
+  if (vertex->last_to_string)
+    free(vertex->last_to_string);
+}
+
+static char const *to_string(Object *self)
+{
+  VertexClass *vertex;
+  char *str;
+  int size;
+
+  vertex = (VertexClass*) self;
+  if (vertex->last_to_string)
+    free(vertex->last_to_string);
+  str = malloc(38 + strlen(vertex->base.__name__));
+  size = sprintf(str, "<%s (%d, %d, %d)>", vertex->base.__name__, vertex->x, vertex->y, vertex->z);
+  vertex->last_to_string = str;
+  str[size] = 0;
+  return (str);
+}
+
+static Object *add_operator(const Object* self, const Object *other)
+{
+  VertexClass *self_vertex;
+  VertexClass *other_vertex;
+  VertexClass *result_vertex;
+
+  self_vertex = (VertexClass*) self;
+  other_vertex = (VertexClass*) other;
+  result_vertex = new(Vertex);
+  result_vertex->x = self_vertex->x + other_vertex->x;
+  result_vertex->y = self_vertex->y + other_vertex->y;
+  result_vertex->z = self_vertex->z + other_vertex->z;
+  return (result_vertex);
+}
+
+static Object *sub_operator(const Object* self, const Object *other)
+{
+  VertexClass *self_vertex;
+  VertexClass *other_vertex;
+  VertexClass *result_vertex;
+
+  self_vertex = (VertexClass*) self;
+  other_vertex = (VertexClass*) other;
+  result_vertex = new(Vertex);
+  result_vertex->x = self_vertex->x - other_vertex->x;
+  result_vertex->y = self_vertex->y - other_vertex->y;
+  result_vertex->z = self_vertex->z - other_vertex->z;
+  return (result_vertex);
+}
+
+static VertexClass _description = {
+    { sizeof(VertexClass), "Vertex", &ctor, &dtor, &to_string,
+    &add_operator, &sub_operator },
+    0, 0, 0, NULL
+};
+
+Class* Vertex = (Class*) &_description;
